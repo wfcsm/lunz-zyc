e3221310e2106b0c3c82ec48c3cf37774ba99637
zyc <627245048@qq.com>
2019-05-26 10:51:04 +0800

    row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col" :class="[`col-${span}`]"></div>
+</template>
+
+<script>
+export default {
+    name:"Lunz",
+    props:{
+        span:{
+            type:[Number,String],
+            default:"auto"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.col{
+    height: 100px;
+    background: gray;
+    width: 50%;
+    border: 1px solid red;
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n}{
+            width: ($n /24)*100%;
+        }
+    }
+    &.col-auto{
+        flex-grow: 1
+    }
+}
+</style>
+
+
