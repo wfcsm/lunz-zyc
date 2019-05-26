83021703d5aff182ff062c4a32360a80923e10b2
zyc <627245048@qq.com>
2019-05-26 12:10:25 +0800

    重构row和col代码

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,9 @@
 <template>
     <div class="col" 
-        :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-        :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+        :class="colClass"
+        :style="colStyle"
         >
-        <div style="border:1px solid red">
             <slot></slot> 
-        </div>
-           
     </div>
 </template>
 
@@ -26,17 +23,26 @@ export default {
         return {
             gutter:0
         }
+    },
+    computed:{
+        colStyle() {
+            return {
+                paddingLeft:this.gutter/2+'px',
+                paddingRight:this.gutter/2+'px'
+            }  
+        },
+        colClass(){
+            let {span,offset} = this;
+            return [span && `col-${span}`, offset && `offset-${offset}`]
+        }
     }
+
 }
 </script>
 
 <style lang="scss" scoped>
 .col{
-    height: 100px;
-    // background: gray;
     width: 50%;
-    // border: 1px solid red;
-
     $class-prefix: col-;
     @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n}{
