e37e0803caa3da7c3eb344bd0ce068191efaccd3
zyc <627245048@qq.com>
2019-06-01 21:56:49 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
-        <slot></slot>
+    <span ref="triggerWrapper" style="display:inline-block">
+      <slot></slot>
     </span>
   </div>
 </template>
@@ -17,30 +17,56 @@ export default {
       visible: false
     };
   },
-  mounted(){
-     
-  },
+  mounted() {},
   methods: {
-    xxx() {
-      this.visible = !this.visible;
-      console.log("切换 visible");
-      if (this.visible === true) {
-          
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper) 
-          let {width,height,top,left}=this.$refs.triggerWrapper.getBoundingClientRect()
-          this.$refs.contentWrapper.style.left = `${left+window.scrollX}px`;
-          this.$refs.contentWrapper.style.top = `${top+window.scrollY}px`
-          
-          let eventHandler = () => {
-            this.visible = false;
-            console.log("document 隐藏popover");
-            document.removeEventListener("click", eventHandler);
-          };
-          document.addEventListener("click", eventHandler);
-        });
-      }else{
-          console.log("切换visible隐藏")
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let {
+        width,
+        height,
+        top,
+        left
+      } = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
+      this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
+    },
+    onClickDocument(e) {
+      if (
+        this.$refs.popover &&
+        (this.$refs.popover === e.target ||
+          this.$refs.popover.contains(e.target))
+      ) {
+        return;
+      }
+      if (
+        this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target ||
+          this.$refs.contentWrapper.contains(e.target))
+      ) {
+        return;
+      }
+      this.close();
+    },
+    open() {
+      this.visible = true;
+      setTimeout(() => {
+        this.positionContent();
+        console.log("监听document");
+        document.addEventListener("click", this.onClickDocument);
+      }, 0);
+    },
+    close() {
+      this.visible = false;
+      console.log("关闭");
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     }
   }
@@ -48,16 +74,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
   position: relative;
 }
- .content-wrapper {
+.content-wrapper {
+  position: absolute;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+//   box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter:drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background:white;
+  transform: translateY(-100%);
+  margin-top: -10px;
+  padding: 0.5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    transform: translateY(-100%);
+    top: 100%;
+    left: 10px;
   }
+  &::before {
+    border-top-color: black;
+  }
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
+}
 </style>
 
